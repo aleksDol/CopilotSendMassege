9056cd249a5e62d3bde4730034025b8ed7cce6ef
aleksDol <dolmatovaleksej855@gmail.com>
2026-03-16 22:51:42 +0300

    Глобальные изменения в чатах3

--- apps/web/components/layout/app-shell.tsx
+++ apps/web/components/layout/app-shell.tsx
@@ -7,7 +7,7 @@ export function AppShell({ children }: { children: React.ReactNode }) {
       <AppSidebar />
       <div className="flex flex-1 flex-col min-h-0 overflow-hidden">
         <AppHeader />
-        <main className="flex min-h-0 flex-1 flex-col overflow-hidden p-4 md:p-8">
+        <main className="flex min-h-0 flex-1 flex-col overflow-hidden">
           {children}
         </main>
       </div>
