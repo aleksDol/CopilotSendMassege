9384a5e4643adc8e19386e82b6d47bcddc508f33
aleksDol <dolmatovaleksej855@gmail.com>
2026-03-20 18:52:00 +0300

    Редизайн чатов 7

--- apps/web/app/(app)/layout.tsx
+++ apps/web/app/(app)/layout.tsx
@@ -5,7 +5,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <AuthGuard>
       <AppShell>
-        <div className="flex min-h-0 flex-1 flex-col overflow-hidden">{children}</div>
+        <div className="flex min-h-0 flex-1 flex-col">{children}</div>
       </AppShell>
     </AuthGuard>
   );
